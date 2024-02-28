fcb3d4f9ef6bf8299dce2675d4f6833a52aa86aa
wikim <wiktormalyska03@gmail.com>
2024-02-29 00:33:43 +0100

    Project Init

---
+++ src/main/java/org/wiktormalyska/bundled_dungeons/BundledDungeons.java
@@ -0,0 +1,10 @@
+package org.wiktormalyska.bundled_dungeons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BundledDungeons implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
---
+++ src/main/java/org/wiktormalyska/bundled_dungeons/client/BundledDungeonsClient.java
@@ -0,0 +1,10 @@
+package org.wiktormalyska.bundled_dungeons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BundledDungeonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
